b011881cd924f679b8dbec755afeb9b15b0bb485
Saeed Maiga <saeedmaiga@Saeeds-MacBook-Air.local>
2025-01-27 00:22:54 -0500

    Added Auth0 integration for login and updated the login

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <App/>
-    </BrowserRouter>
-  </StrictMode>,
-)
+    <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN} // Auth0 domain
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID} // Auth0 client ID
+      authorizationParams={{
+        redirect_uri: window.location.origin, // Redirect URI
+        prompt: "login", // Force login form on every redirect
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
+  </StrictMode>
+);
